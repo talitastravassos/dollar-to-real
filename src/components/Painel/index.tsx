8e806021d788c92b933f1098e1ee7bb22567d503
talitast <talitatravassos.tt@gmail.com>
2019-11-02 11:46:04 -0300

    style: added new font

--- src/components/Painel/index.tsx
+++ src/components/Painel/index.tsx
@@ -75,7 +75,7 @@ export default function Painel() {
       {currencyRate.bid ? (
         <div className={styles.info_container}>
           <p className="has-text-weight-bold is-size-6">
-            Cotação atual do dolar comercial:
+            Cotação atual do Dólar Comercial:
           </p>
           <p className="has-text-weight-bold is-size-5">
             R${" "}
