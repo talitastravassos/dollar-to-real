3df32c2278880a230774941448b73141b35a75fa
talitast <talitatravassos.tt@gmail.com>
2019-11-03 12:59:17 -0300

    add: footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+// Footer Component
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>USD 2 Real</strong> by{" "}
+          <a href="https://github.com/talitastravassos">Talita S. Travassos</a>.
+        </p>
+      </div>
+    </footer>
+  );
+}
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,41 +2,45 @@ import React from "react";
 import ProcessedResult from "./components/ProcessedResult";
 import Painel from "./components/Painel";
 import styles from "./styles.module.scss";
+import Footer from "../../components/Footer";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className="is-size-1 is-size-2-mobile has-text-weight-bold">
-        USD 2 Real
-      </h1>
-      <p className="is-size-5">
-        Simule a compra de dólares americanos, levando em consideração o valor
-        do IOF e a taxa do estado americano. Para realizar uma simulação basta
-        digitar no campo indicado o valor que deseja converter, a taxa do estado
-        americano e selecionar o modo de pagamento (dinheiro ou cartão de
-        crédito), logo abaixo irá aparecer o resultado do cálculo. 
-      </p>
-      <h3
-        className="is-size-4 is-size-5-mobile has-text-weight-bold"
-        style={{ marginTop: 20 }}
-      >
-        {" "}
-        Informações importantes:{" "}
-      </h3>
-      <ul>
-        <li>
-          - Sobre o IOF: é um imposto federal sobre operações de crédito, câmbio
-          e seguros.
-        </li>
-        <li>
-          - Sobre a taxa do estado: nos EUA cada estado tem sua politica de
-          cobrança de impostos sobre transações bancarias.
-        </li>
-      </ul>
-      <div style={{ marginTop: 30 }}>
-        <Painel />
-        <ProcessedResult />
+    <>
+      <div className={styles.container}>
+        <h1 className="is-size-1 is-size-2-mobile has-text-weight-bold">
+          USD 2 Real
+        </h1>
+        <p className="is-size-5">
+          Simule a compra de dólares americanos, levando em consideração o valor
+          do IOF e a taxa do estado americano. Para realizar uma simulação basta
+          digitar no campo indicado o valor que deseja converter, a taxa do
+          estado americano e selecionar o modo de pagamento (dinheiro ou cartão
+          de crédito), logo abaixo irá aparecer o resultado do cálculo.
+        </p>
+        <h3
+          className="is-size-4 is-size-5-mobile has-text-weight-bold"
+          style={{ marginTop: 20 }}
+        >
+          {" "}
+          Informações importantes:{" "}
+        </h3>
+        <ul>
+          <li>
+            - Sobre o IOF: é um imposto federal sobre operações de crédito,
+            câmbio e seguros.
+          </li>
+          <li>
+            - Sobre a taxa do estado: nos EUA cada estado tem sua politica de
+            cobrança de impostos sobre transações bancarias.
+          </li>
+        </ul>
+        <div style={{ marginTop: 30 }}>
+          <Painel />
+          <ProcessedResult />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
